c2ea373ffa4d7625b99d157606ae9ff47d14e336
red2fred2 <red2fred2base@live.com>
2022-02-16 17:48:54 -0600

    Add attack counter

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,11 @@ fn main() {
 	// Set up random number generator
 	let mut rng = rand::thread_rng();
 
-	// Create blank board
-	let board: Vec<usize> = Vec::new();
+	// Create random board to start
+	let mut board = random_board(&mut rng);
+
+
+
 }
 
 /// Returns a board with random data
@@ -25,4 +28,37 @@ fn random_board(rng: &mut ThreadRng) -> Vec<usize> {
 	}
 
 	board
+}
+
+/// Finds the number of queens attacking
+fn queens_attacking(board: &Vec<usize>) -> u8 {
+	let mut attacks = 0;
+	let board_len = board.len();
+
+	// Go column by column
+	for i in 0..board_len {
+		let row = board[i];
+
+		// Check if any queens are in the same row
+		for j in 0..board_len {
+			// If a different column has the same row value
+			if j != i && board[j] == row {
+				attacks += 1;
+			}
+		}
+
+		// Check if any are in a diagonal from here
+		for j in 0..board_len {
+			let vertical_gap = row - board[j];
+
+			if i - j == vertical_gap {
+				attacks += 1;
+			}
+			if j - i == vertical_gap {
+				attacks += 1;
+			}
+		}
+	}
+
+	attacks
 }
