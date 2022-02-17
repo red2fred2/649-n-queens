04ce027ee4cc56dbc6fbca5434a351ea45ce943f
red2fred2 <red2fred2base@live.com>
2022-02-16 18:19:51 -0600

    Add gross print function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ptr;
+
 use rand::{prelude::ThreadRng, Rng};
 
 const BOARD_SIZE: usize = 8;
@@ -9,8 +11,9 @@ fn main() {
 	// Create random board to start
 	let mut board = random_board(&mut rng);
 	let attacks = queens_attacking(&board);
-	println!("attacks: {attacks}");
 
+	println!("attacks: {attacks}");
+	print_board(&board);
 }
 
 /// Returns a board with random data
@@ -31,14 +34,13 @@ fn random_board(rng: &mut ThreadRng) -> Vec<usize> {
 /// Finds the number of queens attacking
 fn queens_attacking(board: &Vec<usize>) -> u8 {
 	let mut attacks = 0;
-	let board_len = board.len();
 
 	// Go column by column
-	for i in 0..board_len {
+	for i in 0..BOARD_SIZE {
 		let row = board[i];
 
 		// Check if any queens are in the same row
-		for j in 0..board_len {
+		for j in 0..BOARD_SIZE {
 			// If a different column has the same row value
 			if j != i && board[j] == row {
 				attacks += 1;
@@ -46,7 +48,7 @@ fn queens_attacking(board: &Vec<usize>) -> u8 {
 		}
 
 		// Check if any are in a diagonal from here
-		for j in 0..board_len {
+		for j in 0..BOARD_SIZE {
 			let vertical_gap = row as isize - board[j] as isize;
 
 			if i as isize - j as isize == vertical_gap {
@@ -61,6 +63,44 @@ fn queens_attacking(board: &Vec<usize>) -> u8 {
 	attacks
 }
 
+/// Prints out a current board
+///
+/// The code is gross, but not important, so I'm not going to make an effort
 fn print_board(board: &Vec<usize>) {
+	let mut print_board: Vec<Vec<bool>> = Vec::new();
 
+	// Set up easier board to print out
+	for r in 0..BOARD_SIZE {
+		let mut row = Vec::new();
+		for c in 0..BOARD_SIZE {
+			if board[c] == r {
+				row.push(true);
+			} else {
+				row.push(false);
+			}
+		}
+		print_board.push(row);
+	}
+
+	// Print board top
+	for _ in 0.. BOARD_SIZE {
+		print!("+---");
+	}
+	println!("+");
+
+	// Print the rest of the board
+	for row in print_board {
+		for col in row {
+			if col {
+				print!("| X ");
+			} else {
+				print!("|   ");
+			}
+		}
+		println!("|");
+		for _ in 0.. BOARD_SIZE {
+			print!("+---");
+		}
+		println!("+");
+	}
 }
